efc8e8a842bbf0d3798e58b94a6dd2234293125c
ajnebro <ajnebro@users.noreply.github.com>
2021-11-06 19:31:40 +0100

    Minor changes

--- DasDennis.py
+++ DasDennis.py
@@ -3,7 +3,7 @@ from typing import List
 import numpy as np
 
 
-class DasDennis():
+class DasDennis:
     def __init__(self, h, m):
         self.h = h
         self.m = m
@@ -35,14 +35,29 @@ class DasDennis():
             next_level = self.__get_generic_level(first_level, previous_level)
             previous_level = next_level
 
-        return self.__get_last_level(previous_level)
+        last_level = self.__get_last_level(previous_level)
+        result = [last_level[i][0] for i in range(len(last_level))]
+        return result
 
+    def save_to_file(self, file_name, weight_vectors, separator=" "):
+        with open(file_name, 'w+') as output_file:
+            for vector in weight_vectors:
+                output_string = ""
+                for value in vector:
+                    output_string += str(value)+ separator
+                output_string = output_string[:-1]
+                output_string += "\n"
+                output_file.write(output_string)
 
-H = 12
+H = 15
 m = 3
 
-weight_vectors = DasDennis(H,m).get_weight_vectors()
+das_dennis = DasDennis(H,m)
+
+weight_vectors = das_dennis.get_weight_vectors()
 
 print("Number of points: " + str(len(weight_vectors)))
 for i in weight_vectors:
     print(i)
+
+das_dennis.save_to_file("W"+str(m)+"D_" + str(len(weight_vectors)) + ".dat", weight_vectors)
